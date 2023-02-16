ef3381715a7ca9f28fc5cdee1b702311cedfa7c3
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2023-02-16 09:35:50 +0000

    First page, alt, aria labels links

--- home-help-services/index.js
+++ home-help-services/index.js
@@ -1,9 +1,12 @@
 const accordianItem = document.querySelectorAll(".accordian-item")
 const answer = document.querySelectorAll(".answer")
-
+const add = document.querySelectorAll(".add")
+const remove = document.querySelectorAll(".remove")
 
 accordianItem.forEach((item, index) => {
     item.addEventListener("click", () => {
         answer[index].classList.toggle("active")
+        add[index].classList.toggle("gone")
+        remove[index].classList.toggle("here")
     })
 })
