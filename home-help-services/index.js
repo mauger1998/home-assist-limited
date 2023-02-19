9c0dcae87888ad7ac2e716981c4dbfee30867dd6
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2023-02-19 21:14:12 +0000

    Swiper cms

--- home-help-services/index.js
+++ home-help-services/index.js
@@ -1,13 +1,60 @@
-const accordianItem = document.querySelectorAll(".accordian-item")
-const answer = document.querySelectorAll(".answer")
-const add = document.querySelectorAll(".add")
-const remove = document.querySelectorAll(".remove")
+// CMS
 
-accordianItem.forEach((item, index) => {
-    item.addEventListener("click", () => {
-        answer[index].classList.toggle("active")
-        add[index].classList.toggle("gone")
-        remove[index].classList.toggle("here")
-    })
+let QUERY = encodeURIComponent(`*[_type == 'question']`);
+
+let URL = `https://1t8q0n0t.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%20%3D%3D%20'question'%5D%0A%20%20`;
+
+fetch(URL)
+        .then((res) => res.json())
+        .then(({ result }) => {
+          const accordian = document.querySelector(".accordian")
+            result.forEach((result, index) => {
+                const accordianItem = document.createElement("div")
+                accordianItem.classList.add("accordian-item")
+                accordian.appendChild(accordianItem)
+                const accordianLink = document.createElement("div")
+                accordianLink.classList.add("accordian-link")
+                accordianLink.textContent = result.question
+                accordianItem.appendChild(accordianLink)
+                const add = document.createElement("p")
+                add.classList.add("add")
+                add.textContent = "+"
+                const remove = document.createElement("p")
+                remove.classList.add("remove")
+                remove.textContent = "-"
+                accordianLink.appendChild(add)
+                accordianLink.appendChild(remove)
+                const answer = document.createElement("div")
+                answer.classList.add("answer")
+                accordianItem.appendChild(answer)
+                const p = document.createElement("p")
+                p.textContent = result.answer
+                answer.appendChild(p)
+ });
+                const accordianItem = document.querySelectorAll(".accordian-item")
+                const answer = document.querySelectorAll(".answer")
+                const plus = document.querySelectorAll(".add")
+                const subtract = document.querySelectorAll(".remove")
+                 accordianItem.forEach((item, index) => {
+                    item.addEventListener("click", () => {
+                        if (answer[index].classList.contains("active")) {
+                            answer[index].classList.remove("active")
+                        
+                        } else {
+                            answer[index].classList.add("active")
+                        }
+
+                        plus[index].classList.toggle("gone")
+                        subtract[index].classList.toggle("here")
+                    })
 })
+
+                        })
+
+
+
+
+
+
+
 
--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const swiper = new Swiper('.swiper', {
     direction: 'horizontal',
     loop: true,
     spaceBetween:10,
-    autoplay: true,
     speed:400,
     // If we need pagination
     pagination: {
@@ -23,15 +22,29 @@ const swiper = new Swiper('.swiper', {
   });
 
 
-  gsap.registerPlugin(ScrollTrigger);
+  // gsap.registerPlugin(ScrollTrigger);
 
-  gsap.to(".second-section h2", {
-      scrollTrigger: {
-          trigger: ".second-section h2",
-          start:"top bottom",
+  // gsap.to(".second-section h2", {
+  //     scrollTrigger: {
+  //         trigger: ".second-section h2",
+  //         start:"top bottom",
          
           
-      },
-      y:0,
-      opacity:1,
-  })
+  //     },
+  //     y:0,
+  //     opacity:1,
+  // })
+
+
+
+  let URLTWO = `https://1t8q0n0t.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%20%3D%3D%20'gallery'%5D%20%7B%0A%20%20%22imageUrl%22%3A%20gallery.asset-%3Eurl%0A%7D%0A%20%20`;
+
+
+  fetch(URLTWO)
+          .then((res) => res.json())
+          .then(({ result }) => {
+            const swiperAll = document.querySelectorAll(".swiper-slide img")
+              result.forEach((result, index) => {
+                  swiperAll[index].src = result.imageUrl
+              })
+          })
