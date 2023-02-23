e0c80253c3f6ac74235577043381bf5614bd4160
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2023-02-23 12:46:46 +0000

    New changes

--- about-home-assist-limited/about.js
+++ about-home-assist-limited/about.js
@@ -23,54 +23,54 @@ const swiper = new Swiper('.swiper', {
   });
 
 
-  let QUERY = encodeURIComponent(`[_type == 'aboutHeroSection']`);
+//   let QUERY = encodeURIComponent(`[_type == 'aboutHeroSection']`);
 
-  let URL = `https://1t8q0n0t.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%20%3D%3D%20'aboutHeroSection'%5D`;
+//   let URL = `https://1t8q0n0t.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%20%3D%3D%20'aboutHeroSection'%5D`;
 
-  fetch(URL)
-        .then((res) => res.json())
-        .then(({ result }) => {
-          const heroHeading = document.querySelector("h1")
-          const heroText = document.querySelector(".main-left > p")
+//   fetch(URL)
+//         .then((res) => res.json())
+//         .then(({ result }) => {
+//           const heroHeading = document.querySelector("h1")
+//           const heroText = document.querySelector(".main-left > p")
           
 
-          heroHeading.textContent = result[0].heading
-          heroText.textContent = result[0].text
+//           heroHeading.textContent = result[0].heading
+//           heroText.textContent = result[0].text
         
-})
+// })
 
-let QUERYTWO = encodeURIComponent(`*[_type == 'aboutSecondSection']`);
+// let QUERYTWO = encodeURIComponent(`*[_type == 'aboutSecondSection']`);
 
-let URLTWO = `https://1t8q0n0t.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%20%3D%3D%20'aboutSecondSection'%5D`;
+// let URLTWO = `https://1t8q0n0t.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%20%3D%3D%20'aboutSecondSection'%5D`;
 
-fetch(URLTWO)
-      .then((res) => res.json())
-      .then(({ result }) => {
-        const secondHeading = document.querySelector(".second-section h2")
-        const secondText = document.querySelector(".second-section p")
+// fetch(URLTWO)
+//       .then((res) => res.json())
+//       .then(({ result }) => {
+//         const secondHeading = document.querySelector(".second-section h2")
+//         const secondText = document.querySelector(".second-section p")
         
 
-        secondHeading.textContent = result[0].heading
-        secondText.textContent = result[0].text
+//         secondHeading.textContent = result[0].heading
+//         secondText.textContent = result[0].text
       
-})
+// })
 
 
-let QUERYFOUR = encodeURIComponent(`*[_type == 'aboutThirdSection']`);
+// let QUERYFOUR = encodeURIComponent(`*[_type == 'aboutThirdSection']`);
 
-let URLFOUR = `https://1t8q0n0t.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%20%3D%3D%20'aboutThirdSection'%5D`;
+// let URLFOUR = `https://1t8q0n0t.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%20%3D%3D%20'aboutThirdSection'%5D`;
 
-fetch(URLFOUR)
-      .then((res) => res.json())
-      .then(({ result }) => {
-        const thirdHeading = document.querySelector(".third-right h2")
-        const thirdSubHeading = document.querySelector(".third-right p:nth-of-type(1)")
-        const thirdText = document.querySelector(".third-right p:nth-of-type(2)")
-        const thirdSecondText = document.querySelector(".third-right p:nth-of-type(3)")
+// fetch(URLFOUR)
+//       .then((res) => res.json())
+//       .then(({ result }) => {
+//         const thirdHeading = document.querySelector(".third-right h2")
+//         const thirdSubHeading = document.querySelector(".third-right p:nth-of-type(1)")
+//         const thirdText = document.querySelector(".third-right p:nth-of-type(2)")
+//         const thirdSecondText = document.querySelector(".third-right p:nth-of-type(3)")
 
-        thirdHeading.textContent = result[0].heading
-        thirdSubHeading.textContent = result[0].subHeading
-        thirdText.textContent = result[0].text
-        thirdSecondText.textContent = result[0].textTwo
+//         thirdHeading.textContent = result[0].heading
+//         thirdSubHeading.textContent = result[0].subHeading
+//         thirdText.textContent = result[0].text
+//         thirdSecondText.textContent = result[0].textTwo
       
-})
+// })
--- home-help-services/index.js
+++ home-help-services/index.js
@@ -1,55 +1,55 @@
 // CMS
 
-// let QUERY = encodeURIComponent(`*[_type == 'question']`);
+let QUERY = encodeURIComponent(`*[_type == 'question']`);
 
-// let URL = `https://1t8q0n0t.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%20%3D%3D%20'question'%5D%0A%20%20`;
+let URL = `https://1t8q0n0t.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%20%3D%3D%20'question'%5D%0A%20%20`;
 
-// fetch(URL)
-//         .then((res) => res.json())
-//         .then(({ result }) => {
-//           const accordian = document.querySelector(".accordian")
-//             result.forEach((result, index) => {
-//                 const accordianItem = document.createElement("div")
-//                 accordianItem.classList.add("accordian-item")
-//                 accordian.appendChild(accordianItem)
-//                 const accordianLink = document.createElement("div")
-//                 accordianLink.classList.add("accordian-link")
-//                 accordianLink.textContent = result.question
-//                 accordianItem.appendChild(accordianLink)
-//                 const add = document.createElement("p")
-//                 add.classList.add("add")
-//                 add.textContent = "+"
-//                 const remove = document.createElement("p")
-//                 remove.classList.add("remove")
-//                 remove.textContent = "-"
-//                 accordianLink.appendChild(add)
-//                 accordianLink.appendChild(remove)
-//                 const answer = document.createElement("div")
-//                 answer.classList.add("answer")
-//                 accordianItem.appendChild(answer)
-//                 const p = document.createElement("p")
-//                 p.textContent = result.answer
-//                 answer.appendChild(p)
-//  });
-//                 const accordianItem = document.querySelectorAll(".accordian-item")
-//                 const answer = document.querySelectorAll(".answer")
-//                 const plus = document.querySelectorAll(".add")
-//                 const subtract = document.querySelectorAll(".remove")
-//                  accordianItem.forEach((item, index) => {
-//                     item.addEventListener("click", () => {
-//                         if (answer[index].classList.contains("active")) {
-//                             answer[index].classList.remove("active")
+fetch(URL)
+        .then((res) => res.json())
+        .then(({ result }) => {
+          const accordian = document.querySelector(".accordian")
+            result.forEach((result, index) => {
+                const accordianItem = document.createElement("div")
+                accordianItem.classList.add("accordian-item")
+                accordian.appendChild(accordianItem)
+                const accordianLink = document.createElement("div")
+                accordianLink.classList.add("accordian-link")
+                accordianLink.textContent = result.question
+                accordianItem.appendChild(accordianLink)
+                const add = document.createElement("p")
+                add.classList.add("add")
+                add.textContent = "+"
+                const remove = document.createElement("p")
+                remove.classList.add("remove")
+                remove.textContent = "-"
+                accordianLink.appendChild(add)
+                accordianLink.appendChild(remove)
+                const answer = document.createElement("div")
+                answer.classList.add("answer")
+                accordianItem.appendChild(answer)
+                const p = document.createElement("p")
+                p.textContent = result.answer
+                answer.appendChild(p)
+ });
+                const accordianItem = document.querySelectorAll(".accordian-item")
+                const answer = document.querySelectorAll(".answer")
+                const plus = document.querySelectorAll(".add")
+                const subtract = document.querySelectorAll(".remove")
+                 accordianItem.forEach((item, index) => {
+                    item.addEventListener("click", () => {
+                        if (answer[index].classList.contains("active")) {
+                            answer[index].classList.remove("active")
                         
-//                         } else {
-//                             answer[index].classList.add("active")
-//                         }
+                        } else {
+                            answer[index].classList.add("active")
+                        }
 
-//                         plus[index].classList.toggle("gone")
-//                         subtract[index].classList.toggle("here")
-//                     })
-// })
+                        plus[index].classList.toggle("gone")
+                        subtract[index].classList.toggle("here")
+                    })
+})
 
-//                         })
+                        })
 
 
 //   let QUERYTWO = encodeURIComponent(`*[_type == 'servicesHeroSection']`);
