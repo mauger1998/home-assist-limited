aa3c303effb0f9cd52fbe006a9ebee243cabedb8
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2023-03-01 19:13:51 +0000

    Add modal

--- index.js
+++ index.js
@@ -19,6 +19,20 @@ fetch(URL)
     
 })
 
+// Modal
+
+const modal = document.querySelector("dialog")
+const openModal = document.querySelector(".open-modal")
+const closeModal = document.querySelector(".close-modal")
+
+openModal.addEventListener("click", () => {
+    modal.show()
+})
+
+closeModal.addEventListener("click", () => {
+    modal.close()
+})
+
 
 
 
