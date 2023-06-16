f4394d6a656e0db3f339ee7622ca9de4d696ef6e
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2023-06-16 08:49:07 +0100

    Push new additions

--- index.js
+++ index.js
@@ -1,29 +1,4 @@
-const URL = "https://1t8q0n0t.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%20%3D%3D%20'latestNews'%5D%0A"
 
-fetch(URL)
-.then((res) => res.json())
-.then(({ result }) => {
-    const newsCards = document.querySelectorAll(".news-card")
-
-    result.forEach((result, index) => {
-        const firstP = document.createElement("p")
-        firstP.textContent = result.cardTitle
-        newsCards[index].appendChild(firstP)
-        const secondP = document.createElement("p")
-        secondP.textContent = result.cardText
-        newsCards[index].appendChild(secondP)
-        const buttonWrapper = document.createElement("div")
-        const enquireButton = document.createElement("button")
-        enquireButton.textContent = "Enquire Now"
-        const buttonLink = document.createElement("a")
-        buttonLink.setAttribute("href", "#contact")
-        buttonLink.appendChild(enquireButton)
-        buttonWrapper.appendChild(buttonLink)
-        newsCards[index].appendChild(buttonWrapper)
-        buttonWrapper.classList.add("bw")
-    })
-    
-})
 
 // Modal
 
