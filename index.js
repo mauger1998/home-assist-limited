2de1f6626893860d11566049f3b9785e88be7fbc
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2023-06-16 09:07:55 +0100

    Add updateable section

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
+let URL = "https://1t8q0n0t.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%20%3D%3D%20%20%22updateable%22%5D%20%7B%0A%20%20%22imgUrl%22%3A%20image.asset-%3Eurl%2C%0A%20%20%20%20title%2C%0A%20%20%20%20paragraph%0A%7D%0A%0A"
 
+fetch(URL)
+.then((res) => res.json())
+.then(({ result }) => {
+    const updateableSection = document.querySelector(".updateable")
+
+    if (result.length > 0) {
+        const title = document.createElement("h2")
+        title.textContent = result[0].title
+        updateableSection.appendChild(title)
+
+        const paragraph = document.createElement("p")
+        paragraph.textContent = result[0].paragraph
+        updateableSection.appendChild(paragraph)
+
+        const button = document.createElement("button")
+        button.textContent = "See All Services"
+        updateableSection.appendChild(button)
+
+    }
+})
+.catch((err) => console.error(err));
 
 // Modal
 
